bf4f09a44981c65b9b5ccf1fcdf2debb24019698
GAYKISSER <vildansamaev@gmail.com>
2024-09-13 16:16:13 +0400

    Version 1

---
+++ Program .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemon
+{
+
+    public abstract class pocemon
+    {
+        public int hp;
+        public int attack;
+        public int defense;
+        public int spesial_attack;
+        public int spesial_defense;
+        public int speed;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
