d18ce3f5aca84c9ee438855cb7650ba12bd31e8e
GAYKISSER <vildansamaev@gmail.com>
2024-09-13 16:17:16 +0400

    Version 1.0.1

--- Program .cs
+++ Program .cs
@@ -16,11 +16,31 @@ namespace pokemon
         public int spesial_defense;
         public int speed;
     }
+    class pokemon_Axew : pocemon
+    {
+        string tip;
+        string name;
+        public pokemon_Axew(string name, string tip, int hp, int attack, int defense, int spesial_attack, int spesial_defense, int speed)
+        {
+            this.name = name;
+            this.tip = tip;
+            this.hp = hp;
+            this.attack = attack;
+            this.defense = defense;
+            this.spesial_attack = spesial_attack;
+            this.spesial_defense = spesial_defense;
+            this.speed = speed;
+            Console.WriteLine($"Имя:{name} Тип:{tip}  ХП:{hp}  Аттака:{attack}  Защита:{defense}  Спец атака:{spesial_attack}  Спец защита:" +
+                $"{spesial_defense}  Скорость:{speed}");
+        }
+    }
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            pokemon_Axew _Axew = new pokemon_Axew("Axew", "dragon", 46, 87, 60, 30, 40, 57);
 
         }
     }
