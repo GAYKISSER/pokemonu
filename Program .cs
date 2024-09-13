bdf9ee3365141967b8b2476ec20dbfa348cdffd3
GAYKISSER <vildansamaev@gmail.com>
2024-09-13 16:19:28 +0400

    Version 1.0.2

--- Program .cs
+++ Program .cs
@@ -20,8 +20,10 @@ namespace pokemon
     {
         string tip;
         string name;
-        public pokemon_Axew(string name, string tip, int hp, int attack, int defense, int spesial_attack, int spesial_defense, int speed)
+
+        public pokemon_Axew(string name, string tip, int hp, int attack, int defense, int spesial_attack, int spesial_defense, int speed, List<string> skills)
         {
+            List<string> skills = new List<string> skills { "Полёт"}
             this.name = name;
             this.tip = tip;
             this.hp = hp;
@@ -30,8 +32,24 @@ namespace pokemon
             this.spesial_attack = spesial_attack;
             this.spesial_defense = spesial_defense;
             this.speed = speed;
-            Console.WriteLine($"Имя:{name} Тип:{tip}  ХП:{hp}  Аттака:{attack}  Защита:{defense}  Спец атака:{spesial_attack}  Спец защита:" +
-                $"{spesial_defense}  Скорость:{speed}");
+
+            foreach (string item_skills in skills)
+
+                Console.WriteLine($"Имя:{name} Тип:{tip}  ХП:{hp}  Аттака:{attack}  Защита:{defense}  Спец атака:{spesial_attack}  Спец защита:" +
+                    $"{spesial_defense}  Скорость:{speed}");
+            Console.WriteLine(item_skills);
+
+        }
+        void imyn_rock(int yron, string tipe_yron)
+        {
+            if (tipe_yron = "rock")
+            {
+                hp - 0;
+            }
+            else
+            {
+                hp - yron;
+            }
         }
     }
 
